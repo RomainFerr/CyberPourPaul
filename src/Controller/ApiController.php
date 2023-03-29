3c7ad33c786f34bced46bddf5a4b14eac6249b60
FERREIRAR <romain.cdesafer@gmail.com>
2023-03-29 17:43:04 +0200

    a

---
+++ src/Controller/ApiController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\CompteRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ApiController extends AbstractController
+{
+    #[Route('/api/compte', name: 'app_api_get', methods: ["GET"])]
+    public function index(CompteRepository $compteRepository): Response
+    {
+        return $this->json($compteRepository->findAll(), 200,[],["groups" => "compte:lire"]);
+    }
+}
